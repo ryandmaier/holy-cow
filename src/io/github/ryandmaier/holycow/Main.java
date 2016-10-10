0f2a18d064a7e7e3ea10e105dea870bfa7d95d78
ryandmaier <ryandmaier@gmail.com>
2016-10-10 15:23:09 -0700

    Person stuff

--- src/io/github/ryandmaier/holycow/Main.java
+++ src/io/github/ryandmaier/holycow/Main.java
@@ -1,12 +1,14 @@
 package io.github.ryandmaier.holycow;
 
 import io.github.ryandmaier.holycow.component.Bull;
+import io.github.ryandmaier.holycow.component.Person;
 import io.github.ryandmaier.holycow.window.MainWindow;
 
 public class Main {
 
     public static void main(String[] args) {
       Engine e = new Engine(800, 600);
+        e.addEntity(new Person(800,600));
       e.start();
     }
 }
--- src/io/github/ryandmaier/holycow/component/Person.java
+++ src/io/github/ryandmaier/holycow/component/Person.java
@@ -7,24 +7,49 @@ import java.awt.geom.RoundRectangle2D;
 
 public class Person extends Entity
 {
-    double aRot;
-    double aSpeed;
+    double bodyX;
+    double bodyY;
+    double bodyW;
+    double bodyH;
+
     private static final double MAX_SPEED = 10;
     private static final double TURN_RADIUS = 5;
     private static final double SPEED_CHANGE = 0.33;
+
     public Person(int width, int height)
     {
         super(width, height);
         
         facingAngle = 0;
         speed = 2;
-        aRot = 0;
-        aSpeed = 0;
+
+        bodyX = 50;
+        bodyY = 50;
+        bodyW = 20;
+        bodyH = bodyW*2;
+
+
     }
-    
+
+
     protected void paintComponent(Graphics g)
     {
-        
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        RoundRectangle2D.Double body = new RoundRectangle2D.Double(bodyX,bodyY,bodyW,bodyH,bodyW/10,bodyH/10);
+        Ellipse2D.Double head = new Ellipse2D.Double(bodyX,bodyY-bodyW,bodyW,bodyW);
+
+
+        g2.setColor(new Color(0xFF7654));
+        g2.fill(body);
+        g2.fill(head);
+
+
+        g2.setStroke(new BasicStroke(5));
+        g2.setColor(Color.BLACK);
+        g2.draw(body);
+        g2.draw(head);
     }
 
 }
